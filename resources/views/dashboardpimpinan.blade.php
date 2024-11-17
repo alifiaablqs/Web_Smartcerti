7b7712b3f5a47b79f9112be609ae54901d6408c7
Shafirasal <shafirasalsabila15@gmail.com>
2024-11-17 16:50:11 +0700

    dashboard sertifikasi dan pelatihan

--- resources/views/dashboardpimpinan.blade.php
+++ resources/views/dashboardpimpinan.blade.php
@@ -7,67 +7,80 @@
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
+          <!-- Total Pelatihan -->
           <div class="col-lg-3 col-6">
-            <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3>{{ $jumlahPelatihan }}</h3> <!-- Tampilkan jumlah total pelatihan -->
 
-                <p>Pelatihan</p>
+                <p>Total Pelatihan </p> <!-- Deskripsi diperjelas -->
               </div>
               <div class="icon">
-              <i class="ion ion-bookmark"></i>
+                <i class="ion ion-bookmark"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ url('pimpinanpelatihandosen') }}" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
             </div>
           </div>
           <!-- ./col -->
+
+          <!-- Total Sertifikasi -->
           <div class="col-lg-3 col-6">
-            <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-              <h3>100</h3>
+                <h3>{{ $jumlahSertifikasi }}</h3> <!-- Tampilkan jumlah total sertifikasi -->
 
-                <p>Sertifikasi</p>
+                <p>Total Sertifikasi</p> <!-- Deskripsi diperjelas -->
               </div>
               <div class="icon">
                 <i class="ion ion-document-text"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ url('pimpinansertifikasidosen') }}" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
             </div>
           </div>
           <!-- ./col -->
+
+          <!-- Pelatihan User -->
           <div class="col-lg-3 col-6">
-            <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-              <h3>{{ $jumlahPelatihan }}</h3> <!-- Tampilkan jumlah data -->
+                <h3>{{ $jumlahPelatihanUser }}</h3> <!-- Tampilkan jumlah pelatihan untuk user -->
 
-                <p>Pelatihan Dosen</p>
+                <p>Pelatihan Anda</p> <!-- Deskripsi untuk user -->
               </div>
               <div class="icon">
                 <i class="ion ion-person-stalker"></i>
               </div>
-              <a href="{{ url('pimpinanpelatihandosen') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ url('pimpinanpelatihandosen') }}" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
             </div>
           </div>
           <!-- ./col -->
+
+          <!-- Sertifikasi User -->
           <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-danger">
-            <div class="inner">
-              <h3>{{ $jumlahSertifikasi }}</h3> <!-- Tampilkan jumlah data -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{ $jumlahSertifikasiUser }}</h3> <!-- Tampilkan jumlah sertifikasi untuk user -->
 
-              <p>Sertifikasi Dosen</p>
+                <p>Sertifikasi Anda</p> <!-- Deskripsi untuk user -->
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-stalker"></i>
+              </div>
+              <a href="{{ url('pimpinansertifikasidosen') }}" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
             </div>
-            <div class="icon">
-              <i class="ion ion-person-stalker"></i>
-            </div>
-            <a href="{{ url('pimpinansertifikasidosen') }}" class="small-box-footer">
-              More info <i class="fas fa-arrow-circle-right"></i>
-            </a>
           </div>
+          <!-- ./col -->
         </div>
-
         <!-- /.row -->
+      </div>
+    </section>
+    <!-- /.content -->
 @endsection
